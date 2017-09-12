31bb326952275a43b3b85a7f20b4a29308aa1aa1
jermyLin <495550623@qq.com>
2017-09-12 15:30:55 +0800

    修改

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 var gulp = require('gulp'), //本地安装gulp所用到的地方
     less = require('gulp-less'),
     autoPrefixer = require('gulp-autoprefixer'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-ruby-sass'),
+    livereload = require('gulp-livereload'),
+    cssmin = require('gulp-clean-css')
 
 //定义一个testLess任务（自定义任务名称）
 gulp.task('testLess', function () {
@@ -15,7 +17,15 @@ gulp.task('testLess', function () {
             //        transform: rotate(45deg);
             remove:true //是否去掉不必要的前缀 默认：true
         }))
-        .pipe(gulp.dest('src/css')); //将会在src/css下生成index.css
+        .pipe(cssmin({
+            advanced: false,//类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+            compatibility: 'ie7',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+            keepBreaks: true,//类型：Boolean 默认：false [是否保留换行]
+            keepSpecialComments: '*'
+            //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+        }))
+        .pipe(gulp.dest('src/css')) //将会在src/css下生成index.css
+        .pipe(livereload())
 });
 gulp.task('sass', () =>
 sass('src/scss/index.scss')
